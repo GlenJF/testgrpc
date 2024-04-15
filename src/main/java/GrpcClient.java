4be0dae0ca63240b5edeb662a48e85fdc82e96ee
GlenJF <152633015+GlenJF@users.noreply.github.com>
2024-04-15 10:22:02 +0100

    This is the initial commit

---
+++ src/main/java/GrpcClient.java
@@ -0,0 +1,4 @@
+
+import org.example.streaming.streamingserverservice.*;
+public class GrpcClient {
+}
---
+++ src/main/java/GrpcServer.java
@@ -0,0 +1,21 @@
+
+import io.grpc.stub.StreamObserver;
+import org.example.streaming.streamingserverservice.*;
+
+import java.util.ArrayList;
+
+public class
+GrpcServer extends StreamingServerServiceGrpc.StreamingServerServiceImplBase  {
+    ArrayList<String> name = new ArrayList<>();
+
+    @Override
+    public void sendUnaryRequest(UnaryRequest request, StreamObserver<UnaryResponse> responseObserver) {
+        String clientName = request.getName();
+        String message = "Hello, " + clientName + "! This is a unary response from the server";
+        UnaryResponse response = UnaryResponse.newBuilder()
+                .setMessage(message)
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
